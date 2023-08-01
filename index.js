6f255e8a5e3cdbb333f87c5e9f4eede995bbcac6
Matinou Badarou <badaroumatinou@gmail.com>
2023-08-01 19:14:38 +0100

    maj index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var selects = document.querySelectorAll('select')
-selects.forEach(select =>{
+/*selects.forEach(select =>{
     select.addEventListener('change',function(e){
         console.log(e.target.options[e.target.selectedIndex].value)
         var valSelected = e.target.options[e.target.selectedIndex].value
         console.log(this.getElementsById(valSelected))
     })
-})
+})*/
 
 var actionsForms = document.querySelectorAll('.action-form')
+
+var options = document.querySelectorAll('option');
+options.forEach(option=>{
+    option.addEventListener('mousedown',function(e){
+        e.target.style.color = 'red'
+    })
+})
